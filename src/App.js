7a0eb9a3d54fb9e2c3c6c2152f5110d8356a71aa
Abduvokhidov Ergashali <147835039+ergashfx2@users.noreply.github.com>
2024-08-12 12:40:36 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import SideBar from "./components/sidebar/sidebar";
 import "./styles/App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AuthProvider } from "./config/auth";
 import PrivateRoute from "./routes/privateRoutes";
-import Login from "./pages/login";
+import login from "./pages/login/login";
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
         <Router>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <SideBar />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
+          <Route path="/login" Component={login} />
+          <PrivateRoute path="/">
+            <SideBar />
+          </PrivateRoute>
         </Router>
       </AuthProvider>
     </div>
